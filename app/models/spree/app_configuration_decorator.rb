ccec13deb5c598ed5f2e68ff7c48a86372bb4ee2
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-20 09:28:44 +0100

    Prevented multiple invocations of self.prepended(base)

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,7 +1,15 @@
 module Spree
   module AppConfigurationDecorator
     
+    # Crucial to prevent multiple invocations of self.prepended(base)!
+    @@already_prepended = false unless defined? @@already_prepended
+
     def self.prepended(base)
+
+      # Crucial to prevent multiple invocations of self.prepended(base)!
+      return if self.class_variable_get(:@@already_prepended)
+      self.class_variable_set(:@@already_prepended, true)
+
       base.preference :stock_items_per_page, :integer, default: 15
     end
 
